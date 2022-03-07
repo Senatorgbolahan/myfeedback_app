6dab94e8eceb0eba3e74482a178f165571df4bcd
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-03-07 12:51:42 +0100

    2 files modified, 1 added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="container">
+      <h1>Hello from header app component!</h1>
+    </header>
+  );
+}
+
+export default Header;
