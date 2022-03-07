5007140e54dee98e249e3115d44eabb398a5abcf
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-03-07 13:07:44 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 export default function App() {
   return (
     <div>
-      <Header/>
+      <Header text='hello OmOgbolahan'/>
       <h1>Hello from the app component!</h1>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Header() {
+function Header({ text }) {
   return (
     <header className="container">
-      <h1>Hello from header app component!</h1>
+      <h1>{text}</h1>
     </header>
   );
 }
